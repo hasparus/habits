cdf6c1ad696c64fe1d657e54966077c10a83db89
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-01-09 19:40:07 +0100

    Monday

--- data.ts
+++ data.ts
@@ -3,34 +3,53 @@ const data: Record<number, Datum[]> = {
     {
       date: "2023-01-01",
       alcohol: 0,
+      instantCoffee: 4,
     },
     {
       date: "2023-01-02",
       alcohol: 0,
+      instantCoffee: 3,
     },
     {
       date: "2023-01-03",
       alcohol: 0,
+      instantCoffee: 2,
     },
     {
       date: "2023-01-04",
       alcohol: 0,
+      instantCoffee: 2,
     },
     {
       date: "2023-01-05",
       alcohol: 0,
+      instantCoffee: 2,
     },
     {
       date: "2023-01-06",
       alcohol: 0,
+      instantCoffee: 4,
     },
     {
       date: "2023-01-07",
       alcohol: 0,
+      instantCoffee: 5,
     },
     {
       date: "2023-01-08",
       alcohol: 0,
+      instantCoffee: 1,
+    },
+    {
+      date: "2023-01-09",
+      alcohol: 0,
+      instantCoffee: 2,
+      coffee: 3,
+    },
+    {
+      date: "2023-01-10",
+      alcohol: 0,
+      instantCoffee: 0,
     },
   ],
 };
@@ -41,4 +60,12 @@ interface Datum {
    * Counted in drinks
    */
   alcohol: number;
+  /**
+   * Counted in cups
+   */
+  instantCoffee: number;
+  /**
+   * Counted in cups, excluding instant coffee
+   */
+  coffee?: number;
 }
